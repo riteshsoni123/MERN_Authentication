c6fbdf5ce9e45e22b0037ff3ec1482e3a8190b19
riteshsoni123 <comedybuddy1@gmail.com>
2022-07-26 18:49:34 +0530

    added the 4 basic routes

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
+app.use("/api/auth", require("./routes/auth"));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
