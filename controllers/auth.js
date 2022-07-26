c3e865bf2ea169554f7b0c1e29748e18e750f586
riteshsoni123 <comedybuddy1@gmail.com>
2022-07-27 01:05:35 +0530

    configured the route for user to login with email and password

--- controllers/auth.js
+++ controllers/auth.js
@@ -22,8 +22,35 @@ exports.register = async (req, res, next) => {
   }
 };
 
-exports.login = (req, res, next) => {
-  res.send("Login Route");
+exports.login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res
+      .status(400)
+      .json({ success: false, error: "Please provide email and password" });
+  }
+
+  try {
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+      res.status(404).json({ success: false, error: "Invalid Credentials" });
+    }
+
+    const isMatch = await user.matchPasswords(password);
+
+    if (!isMatch) {
+      res.status(404).json({ success: false, error: "Invalid credentials" });
+    }
+
+    res.status(200).json({
+      success: true,
+      token: "bazinga",
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
 };
 
 exports.forgotpassword = (req, res, next) => {
